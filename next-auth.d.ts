afedd3b65946660519920a0a72b4fe1293704ba9
samiraby1109-dotcom <samiraby1109@gmail.com>
2026-02-03 08:36:00 -0600

    Update next-auth.d.ts

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,9 +1,19 @@
-import NextAuth, { DefaultSession } from 'next-auth';
+import { DefaultSession } from "next-auth";
 
-declare module 'next-auth' {
+declare module "next-auth" {
   interface Session {
     user: {
       isAdmin?: boolean;
-    } & DefaultSession['user'];
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    isAdmin?: boolean;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    isAdmin?: boolean;
   }
 }
